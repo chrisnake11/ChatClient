e2e7682bcd4f597ce9b0dff901383cc64d48b609
zheyv <2500517199@qq.com>
2025-09-05 11:58:58 +0800

    fix ODR potential error

--- header/singleton.h
+++ header/singleton.h
@@ -1,7 +1,4 @@
-#ifndef SINGLETON_H
-#define SINGLETON_H
-
-#include <memory>
+#pragma once
 #include <mutex>
 #include <iostream>
 
@@ -11,23 +8,27 @@ class Singleton
 protected:
     Singleton() = default;
     Singleton(const Singleton<T>&) = delete;
-    Singleton& operator=(const Singleton<T>&) = delete;
+    Singleton<T>& operator=(const Singleton<T>&) = delete;
     static std::shared_ptr<T> _instance;
+private:
+    static std::once_flag _flag;
 public:
-    static std::shared_ptr<T> getInstance() {
-        static std::once_flag flag;
-        std::call_once(flag, [&] {
-            _instance = std::shared_ptr<T>(new T);
-            });
-        return _instance;
-    }
-    ~Singleton() {
-        std::cout << "~Singleton() " << std::endl;
-    }
-}; 
 
-// definition
+    // 类内自动inline，多DLL加载时，会重复创建对象
+    static std::shared_ptr<T> getInstance();
+    void printAddress()
+    {
+        std::cout << "Singleton address: " << _instance.get() << std::endl;
+    }
+    ~Singleton()
+    {
+        std::cout << "~Singleton()" << std::endl;
+    }
+};
+
 template <typename T>
 std::shared_ptr<T> Singleton<T>::_instance = nullptr;
 
-#endif // SINGLETON_H
+template <typename T>
+std::once_flag Singleton<T>::_flag;
+
